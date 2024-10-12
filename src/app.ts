d4c1213b6895b5c06a18355448a12f91f4733fce
allenlin <ht016746@gmail.com>
2024-10-12 20:24:32 +0700

    chore: not-found and error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 
+import notFound from '@/middlewares/not-found';
+import onError from '@/middlewares/on-error';
+
 const app = new OpenAPIHono();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
+
+app.get('/error', (c) => {
+  throw new Error('test error');
+});
+
+app.notFound(notFound);
+
+app.onError(onError);
 
 export default app;
