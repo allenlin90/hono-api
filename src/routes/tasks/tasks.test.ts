6e3772c53dccf1d4ed572f42a063bd73f8c95a15
allenlin <ht016746@gmail.com>
2024-10-13 17:58:50 +0700

    test: test with hono test helper testClient

--- src/routes/tasks/tasks.test.ts
+++ src/routes/tasks/tasks.test.ts
@@ -6,10 +6,22 @@ import createApp, { createTestApp } from '@/lib/create-app';
 describe('tasks list', () => {
   it('responds with an array', async () => {
     const testRouter = createTestApp(router);
+
     const response = await testRouter.request('/tasks');
     const result = await response.json();
 
     // @ts-expect-error
     expectTypeOf(result).toBeArray();
+  });
+
+  it('responds with an array with hono test helper testClient', async () => {
+    const testApp = createApp();
+    const testRouter = testApp.route('/', router);
+    const client = testClient(testRouter);
+
+    const response = await client.tasks.$get();
+    const json = await response.json();
+
+    expectTypeOf(json).toBeArray();
   });
 });
