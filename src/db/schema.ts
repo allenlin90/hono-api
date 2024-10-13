71bda47c492e41cf8d9109328bfd68cc7970aa28
allenlin <ht016746@gmail.com>
2024-10-13 14:01:39 +0700

    chore: setup default timestamp with Date

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,13 @@
-import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
-const defaultNow = sql`(cast((julianday('now') - 2440587.5)*86400000 as integer))`;
 
 export const tasks = sqliteTable('tasks', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   name: text('name').notNull(),
   done: integer('done', { mode: 'boolean' }).notNull().default(false),
-  createdAt: integer('created_at', { mode: 'timestamp' }).default(defaultNow),
+  createdAt: integer('created_at', { mode: 'timestamp' }).$defaultFn(
+    () => new Date()
+  ),
   updatedAt: integer('updated_at', { mode: 'timestamp' })
-    .default(defaultNow)
+    .$defaultFn(() => new Date())
     .$onUpdate(() => new Date()),
 });
