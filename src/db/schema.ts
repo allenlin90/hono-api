758291e1d6355e64168cba5728eb3caae9a14543
allenlin <ht016746@gmail.com>
2024-10-13 11:23:10 +0700

    chore: setup task schema in drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,14 @@
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const tasks = sqliteTable('tasks', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text('name').notNull(),
+  done: integer('done', { mode: 'boolean' }).notNull().default(false),
+  createdAt: integer('updated_at', { mode: 'timestamp' }).default(
+    sql`cast((julianday('now') - 2440587.5)*86400000 as integer)`
+  ),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(
+    () => new Date()
+  ),
+});
