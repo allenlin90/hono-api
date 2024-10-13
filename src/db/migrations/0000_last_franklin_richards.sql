777f240fd17f81dcf7292bc0a313f84114c52dea
allenlin <ht016746@gmail.com>
2024-10-13 14:13:26 +0700

    chore: use $defaultFn to create schema

--- src/db/migrations/0000_last_franklin_richards.sql
+++ src/db/migrations/0000_last_franklin_richards.sql
@@ -2,6 +2,6 @@ CREATE TABLE `tasks` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
 	`done` integer DEFAULT false NOT NULL,
-	`created_at` integer DEFAULT (cast((julianday('now') - 2440587.5)*86400000 as integer)),
+	`created_at` integer,
 	`updated_at` integer
 );
