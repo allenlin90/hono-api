38f8dcd41f7f67fb74bfedb367832675fa65d901
allenlin <ht016746@gmail.com>
2024-10-13 14:18:19 +0700

    chore: query data from db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,15 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import env from '@/env';
+import * as schema from '@/db/schema';
 
 const client = createClient({
   url: env.DATABASE_URL,
   authToken: env.DATABASE_AUTH_TOKEN,
 });
 
-const db = drizzle(client);
+const db = drizzle(client, {
+  schema,
+});
 
 export default db;
--- src/routes/tasks/tasks.handlers.ts
+++ src/routes/tasks/tasks.handlers.ts
@@ -1,11 +1,8 @@
+import db from '@/db';
 import type { AppRouteHandler } from '@/lib/types';
 import type { ListRoute } from '@/routes/tasks/tasks.routes';
 
-export const list: AppRouteHandler<ListRoute> = (c) => {
-  return c.json([
-    {
-      name: 'Learn Hono',
-      done: false,
-    },
-  ]);
+export const list: AppRouteHandler<ListRoute> = async (c) => {
+  const tasks = await db.query.tasks.findMany();
+  return c.json(tasks);
 };
