88977b9351b89b9b0d8a8c3984d4bdd0ab5a4e38
allenlin <ht016746@gmail.com>
2024-10-13 11:08:35 +0700

    chore: db env setup

--- src/env.ts
+++ src/env.ts
@@ -4,11 +4,20 @@ import { z, type ZodError } from 'zod';
 
 expand(config());
 
-const EnvSchema = z.object({
-  NODE_ENV: z.string().default('development'),
-  PORT: z.coerce.number().default(3000),
-  LOG_LEVEL: z.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']), // pino.level
-});
+const EnvSchema = z
+  .object({
+    NODE_ENV: z.string().default('development'),
+    PORT: z.coerce.number().default(3000),
+    LOG_LEVEL: z.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']), // pino.level
+    DATABASE_URL: z.string().url(),
+    DATABASE_AUTH_TOKEN: z.string().optional(),
+  })
+  .refine((input) => {
+    if (input.NODE_ENV === 'production') {
+      return !!input.DATABASE_AUTH_TOKEN;
+    }
+    return true;
+  });
 
 export type Env = z.infer<typeof EnvSchema>;
 
