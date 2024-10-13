b414e06178e4b1c1e3a02b1a303775ad336a6a45
allenlin <ht016746@gmail.com>
2024-10-13 14:51:49 +0700

    chore: create task handler

--- src/routes/tasks/tasks.handlers.ts
+++ src/routes/tasks/tasks.handlers.ts
@@ -1,8 +1,15 @@
 import db from '@/db';
+import { tasks } from '@/db/schema';
 import type { AppRouteHandler } from '@/lib/types';
-import type { ListRoute } from '@/routes/tasks/tasks.routes';
+import type { CreateRoute, ListRoute } from '@/routes/tasks/tasks.routes';
 
 export const list: AppRouteHandler<ListRoute> = async (c) => {
   const tasks = await db.query.tasks.findMany();
   return c.json(tasks);
+};
+
+export const create: AppRouteHandler<CreateRoute> = async (c) => {
+  const task = c.req.valid('json');
+  const [inserted] = await db.insert(tasks).values(task).returning();
+  return c.json(inserted);
 };
