e773e1913941289563182d3fa64fda07b9f436fe
allenlin <ht016746@gmail.com>
2024-10-13 19:31:11 +0700

    chore: export router type for rpc client

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ import tasks from '@/routes/tasks/tasks.index';
 
 const app = createApp();
 
-const routes = [index, tasks];
+const routes = [index, tasks] as const;
 
 configureOpenAPI(app);
 
 routes.forEach((route) => {
   app.route('/', route);
 });
+
+export type AppType = (typeof routes)[number];
 
 export default app;
